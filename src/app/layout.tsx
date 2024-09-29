8d49aa1b6e56696e8b38187a5e5823bfba7fedc0
Md. Soyeb Islam <soyeb10191@gmail.com>
2024-09-29 20:32:13 +0600

    Implement messagecard and delete-message route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import AuthProvider from "./context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
+import AuthProvider from "./context/AuthProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
